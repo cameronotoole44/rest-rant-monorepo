6bd3514bfeafccd97958262528327ed50ec91a61
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-29 12:28:27 -0400

    added auth

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,9 @@
 require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
+const cors = require('cors');
 const app = express();
+
 
 // Express Settings
 app.use(cors())
@@ -11,12 +12,14 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+
 // Controllers & Routes
 
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
