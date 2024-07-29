e41a177637230c4a9034fd78eef1e91328b5874e
cameronotoole44 <cameronotoole404@gmail.com>
2024-07-29 12:28:45 -0400

    added password digest

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,26 +1,22 @@
-const router = require('express').Router();
-const db = require("../models");
-const bcrypt = require('bcrypt');
-const jwt = require('json-web-token');
+const router = require('express').Router()
+const db = require("../models")
+const bcrypt = require('bcrypt')
 
-const { User } = db;
+const { User } = db
 
 router.post('/', async (req, res) => {
-
-    let user = await User.findOne({
-        where: { email: req.body.email }
+    let { password, ...rest } = req.body;
+    const user = await User.create({
+        ...rest,
+        passwordDigest: await bcrypt.hash(password, 10)
     })
-
-    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({ message: `Could not find a user with the provided username and password` })
-    } else {
-        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
-        res.json({ user: user, token: result.value })
-    }
+    res.json(user)
 })
 
-router.get('/profile', async (req, res) => {
-    res.json(req.currentUser)
-});
 
-module.exports = router;
+router.get('/', async (req, res) => {
+    const users = await User.findAll()
+    res.json(users)
+})
+
+module.exports = router
